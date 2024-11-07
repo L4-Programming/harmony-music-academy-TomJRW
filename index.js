b6ac2ec5316a79d3cde7946cf254bd0af11202b5
TomJRW <1740305@hc.ac.je>
2024-11-07 11:09:34 +0000

    comment plan
    added plan 4 frm logic

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-// javascript
-
-console.log("Hello from index.js!");
+// Display a form to the user
+// Include input fields for the user's email address, level, and hours of study
+// Capture user's input on form submission
+// Get the user's email address - userEmail (string)
+// Get the user's level - userLevel (string)
+// Get the user's hours of study - userHours (number)
+// Validate the user's input
+// Check if the user has selected a level
+// Check if the user has provided an email address - has it entrd, @check
+// Check if the user has specified at least one hour of study 0 cnnt b negative
+// Check if the number of hours requested is within the allowed range - must be = to or > than 1, less than or = to max numb for the level
+// Calculate the total cost
+// Calculate the total cost
+// Display the total cost to the user
